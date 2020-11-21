67ad126c4c73c1a4a319ccdb231c506b070baca6
luc <luc.bourrat.42@gmail.com>
2020-11-21 03:45:17 +0100

    add player constructor - map constructor - and 2 map methods

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 import Map from './map.js';
 import Player from './player.js';
 import Weapon from './weapon.js';
+
+let Ogre = new Player("Ogre", 100);
+let Elfe = new Player("Elfe", 100);
+Ogre.sayHi();
+Elfe.sayHi();
+
+let gameMap = new Map();
+gameMap.sayHi();
--- js/player.js
+++ js/player.js
@@ -1,3 +1,10 @@
 export default class Player {
+	constructor(name, hp) {
+		this.name = name;
+		this.hp = hp;
+	}
 
+	sayHi() {
+		console.log("Bonjour ! Je suis " + this.name + ", j'ai " + this.hp + " points de vie");
+	}
 }
